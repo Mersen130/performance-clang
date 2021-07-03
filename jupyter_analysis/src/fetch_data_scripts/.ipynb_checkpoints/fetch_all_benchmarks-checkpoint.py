41868e335397aa978915d19f055b1c80308b4db8
Mersen130 <mersenyip0130@outlook.com>
2021-07-04 01:30:15 +0800

    Update script

--- jupyter_analysis/src/fetch_data_scripts/.ipynb_checkpoints/fetch_all_benchmarks-checkpoint.py
+++ jupyter_analysis/src/fetch_data_scripts/.ipynb_checkpoints/fetch_all_benchmarks-checkpoint.py
@@ -8,12 +8,15 @@ remote = "http://104.154.54.203"
 
 class MyHTMLParser(HTMLParser):
     count = 0
+    tag = True
+    data = None
 
     def handle_starttag(self, tag, attrs):
         if tag == "a" and attrs and "graph" in attrs[0][1] and attrs[0][1][0] == '/':
+            self.tag = True
             
-            if self.count >= 5:
-                exit(0)
+#             if self.count >= 5:
+#                 exit(0)
             self.count += 1
             print("Starting {}: {}".format(self.count, "MODE=text\nURL={}".format(remote + attrs[0][1])))
             
@@ -21,9 +24,17 @@ class MyHTMLParser(HTMLParser):
             with open(".env", "w") as f:
                 f.write("MODE=text\nURL={}".format(remote + attrs[0][1]))
             filename = str(int(hashlib.sha256(attrs[0][1].encode('utf-8')).hexdigest()[:16], 16)-2**63)
-            os.system("python3 fetch_1benchmark.py > {}".format("results/" + filename))
 
+            
+    def handle_endtag(self, tag):
+        if self.tag:
+            os.system("python3 fetch_1benchmark.py > {}".format("results/" + self.data))
             print("{} Done".format(self.count))
+        self.tag = False
+        
+    def handle_data(self, data):
+        if self.tag:
+            self.data = data.split("/")[-1]
 
 
 
